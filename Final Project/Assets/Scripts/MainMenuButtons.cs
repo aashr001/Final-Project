9bdf0cff98d84b4eedc6a071f872ae54bbd629ca
Aneeza Ashraf <aashr001@gold.ac.uk>
2018-05-07 16:46:58 +0100

    First Commit
    Uploading all my files to GitHub

---
+++ Final Project/Assets/Scripts/MainMenuButtons.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuButtons : MonoBehaviour {
+
+    public string nextLevel;
+    public Texture2D originalTexture;
+    public Texture2D highlightedTexture;
+    //public AudioClip clickSound;
+
+    private new GUITexture guiTexture;
+
+    void Start()
+    {
+        guiTexture = GetComponent<GUITexture>();
+    }
+
+    void OnMouseEnter()
+    {
+        guiTexture.texture = highlightedTexture;
+    }
+
+    void OnMouseExit()
+    {
+        guiTexture.texture = originalTexture;
+    }
+
+
+}
+	
+
