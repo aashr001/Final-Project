ba6249f03feb7e49594e59d8a6b0e380ab03ae9f
Aneeza Ashraf <aashr001@gold.ac.uk>
2018-05-14 16:52:10 +0100

    Done testing, everything works

---
+++ Final Project/Assets/Scripts/ButtonChange.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+
+
+public class ButtonChange : MonoBehaviour
+{
+    public Sprite originalImage;
+    public Sprite highlightedImage;
+
+    private void Start()
+    {
+        originalImage = GetComponent<Sprite>();
+    }
+    void OnMouseEnter()
+    {
+        originalImage = highlightedImage;
+
+    }
+
+    void OnMouseExit()
+    {
+        highlightedImage = originalImage;
+    }
+}
+
+
+
