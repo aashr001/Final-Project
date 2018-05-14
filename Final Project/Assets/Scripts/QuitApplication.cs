fadcfb8690178e5fe2aa3ec62e12f8c87aaaecf0
Aneeza Ashraf <aashr001@gold.ac.uk>
2018-05-14 10:12:20 +0100

    Hogwarts lights
    created an animation for Hogwarts

---
+++ Final Project/Assets/Scripts/QuitApplication.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitApplication : MonoBehaviour {
+
+    public void QuitApp()
+    {
+        //if (Input.GetKey("escape"))
+        //{ 
+        //Application.Quit();
+        //}
+
+        //else
+        //{
+        //  Application.Quit();
+        //}
+        Application.Quit();
+        
+    }
+    
+}
