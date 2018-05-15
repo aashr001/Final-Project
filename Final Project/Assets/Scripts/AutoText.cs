16f89040a8ccaffd30df5c3418b8877b12e32d91
Aneeza Ashraf <aashr001@gold.ac.uk>
2018-05-15 08:04:28 +0100

    FINAL VERSION OF GAME

--- Final Project/Assets/Scripts/AutoText.cs
+++ Final Project/Assets/Scripts/AutoText.cs
@@ -5,8 +5,11 @@ using UnityEngine.UI;
 
 public class AutoText : MonoBehaviour {
 
+    //how long the delay should be
     public float wait = 0.5f;
+    //the whole text to be displayed
     public string wholeText;
+    //current text position
     private string currentText = "";
 
 	// Use this for initialization
@@ -15,15 +18,16 @@ public class AutoText : MonoBehaviour {
 		
 	}
 	
+    //to read and display the text
     IEnumerator DisplayText()
     {
         int i = 0;
-        while (i < wholeText.Length)
+        while (i < wholeText.Length)//value of i is less than length of text
         {
-            currentText = wholeText.Substring(0, i);
+            currentText = wholeText.Substring(0, i);//start at 0 index, end at value of i
             this.GetComponent<Text>().text = currentText;
             i++;
-            yield return new WaitForSeconds(wait);
+            yield return new WaitForSeconds(wait);//speed of writing
             
         }
 
